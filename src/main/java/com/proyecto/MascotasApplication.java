80b3a5eb6f06e1af7940cf8eae5a8158745efdd3
carlosmendez98 <carlosvalenciamendez@gmail.com>
2024-07-15 17:47:30 -0500

    Se actualiza con flyway

--- src/main/java/com/proyecto/MascotasApplication.java
+++ src/main/java/com/proyecto/MascotasApplication.java
@@ -2,12 +2,6 @@ package com.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*
-
-Mascotas("/mascota") => debe retornar una lista de mascotas
-Mascotas("/mascota/{id}") => debe retornar una mascota
-Adoptante(/adoptantes/{user_id}) = debe retornar un adoptante
-*/
 
 
 @SpringBootApplication
